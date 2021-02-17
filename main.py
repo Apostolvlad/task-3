16785d69c5e99d19e059f49dd9735af956765048
Влад Ульянский <vlad13579@inbox.ru>
2021-02-17 18:26:58 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, Side, Color
+import requests
+from bs4 import BeautifulSoup
+
+FILL_SPLIT = PatternFill(fill_type='solid', fgColor= Color(rgb='cfb53b'))
+FILL_TITLE = PatternFill(fill_type='solid', fgColor= Color(rgb='42aaff'))
+
+def set_fill(ws, i, color):
+    for y in ws[f'A{i}:G{i}'][0]:
+        y.fill = color
+
+def parsing(ws, url):
+    soup = BeautifulSoup(requests.get(url).text, features="html.parser")
+    h1 = soup.h1.text.replace('\n', '').lstrip().rstrip()
+
+    data_section = soup.find('a', class_ = 'js-popup-open button big invert w250')['data-section']
+    data_con = soup.find('div', class_ = 'org-heading-right')['data-con']
+    for master in soup.find_all('div', class_ = 'executors-block type2'):
+        for zakaz in master.find_all('div', class_ = 'org-table-row'):
+            if not zakaz.get('data-f', '').startswith('f-'): continue
+            description = zakaz.find('div', class_ = 'org-table-col short-desc')
+            description_full = zakaz.find('div', class_ = 'org-table-row-hidden-text description full-desc')
+            if not description is None: description = description.text.replace('\n', '').lstrip().rstrip()
+            if not description_full is None: description_full = description_full.text.replace('\n', '').lstrip().rstrip()
+            #print()
+            ws.append([url, h1, description, description_full, data_section, data_con, zakaz.get('data-f')])
+    ws.append([])
+    set_fill(ws, ws.max_row + 1, FILL_SPLIT)
+
+
+
+def main():
+    with open('urls.txt') as f: base_urls = tuple(f.read().split('\n'))
+    wb = Workbook()
+    ws = wb.active
+    ws.title = 'Результат'
+    ws.append(['URL', 'H1', 'Краткое описание', 'Описание', 'ID sec', 'ID cont', 'ID заказа'])
+    set_fill(ws, 1, FILL_TITLE)
+    for url in base_urls:
+        parsing(ws, url)
+    wb.save("result.xlsx")
+
+if __name__ == '__main__':
+    main()
+    
